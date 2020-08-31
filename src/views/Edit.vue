2e869311f42120d28fbdec0db4b1f5dfec082e43
hino <hino@freedomland.jp>
2020-08-31 13:54:15 +0900

    【Vue.js】メモ帳アプリ作成

---
+++ src/views/Edit.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="editor">
+    <h1>Edit Memo</h1>
+    <textarea name="memo" v-model="memo.body"></textarea>
+    <button>保存</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'edit',
+  computed: {
+    memo: function() {
+      let id = this.$route.params["id"];
+      return this.$store.state.memos.find(memo => memo.id == id);
+    }
+  },
+  methods: {
+    save: function() {
+      this.$store.commit("update", {
+        id: this.memo.id,
+        body: this.memo.body
+      });
+      this.$router.push('/');
+    }
+  }
+}
+</script>
---
+++ src/views/New.vue
@@ -0,0 +1,46 @@
+<template>
+<!-- template内には１つしかタグを入れることが出来ない為divで囲う必要がある -->
+  <div class="new">
+    <h1>New Memo</h1>
+    <textarea name="memo" v-model="memoBody" id="" cols="" rows=""></textarea>
+    <button @click="save">保存</button>
+  </div>
+</template>
+
+<style scoped>
+textarea {
+  width: 100%;
+  height: 10em;
+  margin-bottom: 10px;
+}
+
+button {
+  border: 1px solid #333;
+  background-color: #333;
+  color: #fff;
+  padding: 10px 20px;
+}
+
+</style>
+<script>
+export default {
+  name: "new",
+  data: function() {
+    return {
+      memoBody: ""
+    }
+  },
+  methods: {
+    save: function() {
+      // alert('保存されました');
+      this.$store.commit("save", {
+        body: this.memoBody
+      });
+
+      this.$router.push("/")
+    }
+  }
+
+}
+
+</script>
